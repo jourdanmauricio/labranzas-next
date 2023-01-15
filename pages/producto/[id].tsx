6e2851773282c9efb7ccba1213bdcc3a34dedae4
mauricio <jourdanmauricio@gmail.com>
2023-01-15 13:14:05 -0300

    Add Spinner

--- pages/producto/[id].tsx
+++ pages/producto/[id].tsx
@@ -1,7 +1,5 @@
 import Head from 'next/head';
-import Image from 'next/image';
 import { Inter } from '@next/font/google';
-import styles from '@/styles/Home.module.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -11,9 +9,7 @@ export default function Home() {
       <Head>
         <title>Labranzas | Producto</title>
       </Head>
-      <main className={styles.main}>
-        <h1 className="text-4xl">Este es el producto ...</h1>
-      </main>
+      <h1 className="text-4xl">Este es el producto ...</h1>
     </>
   );
 }
