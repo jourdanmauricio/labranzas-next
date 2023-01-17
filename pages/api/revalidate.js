bc86fe2375a9e12a613c351198a67cd1d068dc3a
mauricio <jourdanmauricio@gmail.com>
2023-01-17 12:36:58 -0300

    Add revalidate on demand

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -6,6 +6,7 @@
 // };
 
 export default async function handler(req, res) {
-  await res.revalidate('/');
+  // await res.revalidate('/');
+  await res.revalidate;
   res.status(200).json({ revalidate: true });
 }
