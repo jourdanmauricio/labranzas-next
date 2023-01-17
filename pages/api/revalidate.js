5e948b8de62b8a2f6bd473b2ddaa83535899a76e
mauricio <jourdanmauricio@gmail.com>
2023-01-17 12:18:40 -0300

    Add revalidate on demand

---
+++ pages/api/revalidate.js
@@ -0,0 +1,11 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// import type { NextApiRequest, NextApiResponse } from 'next';
+
+// type Data = {
+//   name: string,
+// };
+
+export default async function handler(req, res) {
+  await res.revalidate('/');
+  res.status(200).json({ revalidate: true });
+}
